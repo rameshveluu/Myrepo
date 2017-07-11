ccb508130880f89376deb5aa0cef6102e8b35ce5
EC2 Default User <ec2-user@ip-172-31-27-147.ec2.internal>
2017-07-11 06:49:36 +0000

    mess

--- user_policyscript.sh
+++ user_policyscript.sh
@@ -1,4 +1,4 @@
 for i in $(cat employee.txt);do
-echo aws iam create-user --user-name $i,
-echo aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --user-name $i
+aws iam create-user --user-name $i,
+aws iam attach-user-policy --policy-arn arn:aws:iam::aws:policy/AdministratorAccess --user-name $i
 done 
